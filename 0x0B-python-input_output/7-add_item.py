7aa848c7019a225f5b605fefc9e18bb6a0b769c5
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-02-07 12:38:31 -0500

    Fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""7-add_item.py
-script that adds all arguments to a Python list, and then save them to a file:
-"""
-
+"""Program to add load and save a add_item.json file"""
 
 import sys
-import os.path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
 
-if os.path.isfile(filename):
-    json_file = load_from_json_file(filename)
-    json_list = json_file + sys.argv[1:]
-    save_to_json_file(json_list, filename)
-else:
-    save_to_json_file(sys.argv[1:], filename)
+if __name__ == "__main__":
+
+    save = __import__('5-save_to_json_file').save_to_json_file
+    load = __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        l_items = load("add_item.json")
+    except FileNotFoundError:
+        l_items = []
+
+    l_items.extend(sys.argv[1:])
+    save(l_items, "add_item.json")
