b3235780838b09b38e01a9293575d5ba48df0bd3
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-02-07 12:36:21 -0500

    7Add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""This module appends data to a json file"""
+"""7-add_item.py
+script that adds all arguments to a Python list, and then save them to a file:
+"""
+
 
 import sys
-import json
+import os.path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
 
-def main():
-    """main function that does the things"""
-    filename = "add_item.json"
-    try:
-        finit = load_from_json_file(filename)
-    except:
-        finit = []
-    finit += sys.argv[1:]
-    save_to_json_file(finit, filename)
-
-
-if __name__ == "__main__":
-    main()
+if os.path.isfile(filename):
+    json_file = load_from_json_file(filename)
+    json_list = json_file + sys.argv[1:]
+    save_to_json_file(json_list, filename)
+else:
+    save_to_json_file(sys.argv[1:], filename)
