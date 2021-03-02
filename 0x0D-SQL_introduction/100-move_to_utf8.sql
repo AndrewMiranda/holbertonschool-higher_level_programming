cfd6a381a04fe6e03da923c748509dab734c7256
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-03-01 20:05:45 -0500

    100Move

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,2 @@
 -- converts hbtn_0c_0 database to UTF8mb4
-ALTER DATABASE dbname CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
