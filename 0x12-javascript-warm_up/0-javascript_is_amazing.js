451f9addce8e13950f3c5c782677518a718fdd0a
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-03-22 09:27:39 -0500

    Standard fix

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node 
-// Write a script that prints “JavaScript is amazing”
-// myVar: constant variable
+/*
+* Write a script that prints “JavaScript is amazing”
+* myVar: constant variable
+*/
+
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
