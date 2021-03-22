9b76aabe4250c7c279a47e683fc53c9a630a6fa2
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-03-22 16:04:54 -0500

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
-
+#!/usr/bin/node
+/* Script that print 'x' times "C is fun" */
+const num = parseInt(process.argv[2]);
+if (num === undefined || isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
