c65b405526dc0815461085b203d51a8bcaeb9979
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-03-22 16:07:29 -0500

    Update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,10 @@
-
+#!/usr/bin/node
+/* Script that prints a square with the letter 'X' */
+const num = parseInt(process.argv[2]);
+if (num === undefined || isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
+}
