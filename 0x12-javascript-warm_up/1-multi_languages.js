4045e20e8537dedea0fafd75e824eb03ba5cb9a8
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-03-22 13:38:59 -0500

    Fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,3 @@
-#!/usr/bin/node 
-/*
-* Write a script that prints 3 lines
-*/
-
+#!/usr/bin/node
+/* Write a script that prints 3 lines */
 console.log('C is fun' + '\n'+'Python is cool' + '\n' + 'JavaScript is amazing' + '\n')
