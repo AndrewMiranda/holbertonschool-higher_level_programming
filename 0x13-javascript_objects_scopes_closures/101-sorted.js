68302127038f83b33455783b1c8815479efe0ae5
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-03-23 21:24:41 -0500

    101Sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
-exports.dict = {
-  89: 1,
-  90: 2,
-  91: 1,
-  92: 3,
-  93: 1,
-  94: 2
-};
+/* SCript that imports a dictionary of ocurrences by user id and computes a
+ * dictionary of user ids by ocurrence */
+
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const [key, value] of Object.entries(dict)) {
+  if (newDict[value] === undefined) {
+    newDict[value] = [key];
+  } else {
+    newDict[value].push(key);
+  }
+}
+console.log(newDict);
