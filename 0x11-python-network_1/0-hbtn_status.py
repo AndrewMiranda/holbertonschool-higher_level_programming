78f435cfb8e01557b70f79e684ecb54f0d433f8a
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-05-28 13:04:25 -0500

    0Hbtn

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" Script: Fetches a website and show the response
 """
-import urllib.request
-
+Python script that fetches https://intranet.hbtn.io/status
+"""
+from urllib import request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
 
-    print("Body response:\n\
-\t- type: {}\n\
-\t- content: {}\n\
-\t- utf8 content: {}".format(type(html), html, html.decode("utf-8"))
+    req = request.Request("https://intranet.hbtn.io/status")
+
+    with request.urlopen(req) as response:
+        body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
